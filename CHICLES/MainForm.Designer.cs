f6c74a6c3e597feb9c5e13f2054d97f91c0a9aae
AlejandroCT-a11y <cata090728hgtlnla0@cbtis217.edu.mx>
2025-12-11 14:01:23 -0600

    postres,Actualizado

--- CHICLES/MainForm.Designer.cs
+++ CHICLES/MainForm.Designer.cs
@@ -311,7 +311,7 @@ namespace CHICLES
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
 			this.ClientSize = new System.Drawing.Size(645, 523);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.rbDulces);
